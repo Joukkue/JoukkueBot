626fb5c7fbbb3b4b32e9833b157436a96b492d5c
joel arantola <joel.arantola@outlook.com>
2017-05-11 19:53:58 +0300

    [edit] remove whitespace

--- vertaa.py
+++ vertaa.py
@@ -2,20 +2,33 @@
 
 class comparing():
     def __init__(self, line):
-        self.line = line
+        self.removewhitespace(line)
         
     def __del__(self):
         return
+    
+    def removewhitespace(self, line):
+        a = []
+        for i in range (len(line)):
+            if line[i] != " ":
+                a.append(line[i])
+        kasa = ""
+        for i in range (len(a)):
+            kasa+=a[i]
+        self.line = kasa
         
     def is_tuli(self):
-        kasa = self.line.rstrip()
-        kasa = kasa.lower()
+        tulemisarvo = False
+        kasa = self.line.lower()
         if len(kasa)>=4:
             for i in range (len(kasa)-3):
                 print(i)
                 line = kasa[i:i+4]
+                print(line)
                 if line == "tuli":
                     tulemisarvo = True
+                    del(self)
+                    return True
         else:
             tulemisarvo = False
         del(self)
