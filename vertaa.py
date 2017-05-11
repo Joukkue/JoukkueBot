478f9d4c768fa9d3c489fd8f8dd8a6c0dd02917c
Joonas Malinen <joonas.malinen@hotmail.fi>
2017-05-11 19:56:01 +0300

    Revert "Merge remote-tracking branch 'origin/tulitesti' into tulitesti"
    This reverts commit c161fa53452bdb5a45a10fc3f1bfd12a5502875e.

--- vertaa.py
+++ vertaa.py
@@ -2,33 +2,20 @@
 
 class comparing():
     def __init__(self, line):
-        self.removewhitespace(line)
+        self.line = line
         
     def __del__(self):
         return
-    
-    def removewhitespace(self, line):
-        a = []
-        for i in range (len(line)):
-            if line[i] != " ":
-                a.append(line[i])
-        kasa = ""
-        for i in range (len(a)):
-            kasa+=a[i]
-        self.line = kasa
         
     def is_tuli(self):
-        tulemisarvo = False
-        kasa = self.line.lower()
+        kasa = self.line.rstrip()
+        kasa = kasa.lower()
         if len(kasa)>=4:
             for i in range (len(kasa)-3):
                 print(i)
                 line = kasa[i:i+4]
-                print(line)
                 if line == "tuli":
                     tulemisarvo = True
-                    del(self)
-                    return True
         else:
             tulemisarvo = False
         del(self)
