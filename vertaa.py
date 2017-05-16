11bc2990acc7b56a86ae70306b645645833c95c3
joel arantola <joel.arantola@outlook.com>
2017-05-17 00:11:42 +0300

    Siivottu koodia

--- vertaa.py
+++ vertaa.py
@@ -18,29 +18,20 @@ class comparing():
         self.line = kasa
         
     def is_tuli(self):
-        tulemisarvo = False
-        kasa = self.line.lower()
         if len(kasa)>=4:
             for i in range (len(kasa)-3):
                 line = kasa[i:i+4]
                 if line == "tuli":
-                    tulemisarvo = True
-                    return tulemisarvo
-        else:
-            tulemisarvo = False
-        return tulemisarvo
+                    return True
+        return False
     
     def is_tissit(self):
-        tissivakio = False
         kasa = self.line.lower()
         if len(kasa)>=6:
             for i in range (len(kasa)-5):
                 line = kasa[i:i+6]
                 if line == "tissit" or line == "715517":
-                    tissivakio = True
                     del(self)
-                    return tissivakio
-        else:
-            tissivakio = False
+                    return True
         del(self)
-        return tissivakio
+        return False
