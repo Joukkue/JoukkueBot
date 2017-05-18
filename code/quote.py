d70a9907638596a16f8dfd4e923e16b89d2c3b6e
Joonas Malinen <joonas.malinen@hotmail.fi>
2017-05-18 16:56:39 +0300

    [IMP] Quotes work fine

--- code/quote.py
+++ code/quote.py
@@ -1,9 +1,28 @@
 from __future__ import print_function
 
+counter = -1
+
+def fileLength():
+    stack = open("quotes.txt", "r")
+    global  counter
+    line = "lel"
+    while line!="":
+        line = stack.readline()
+        line = line.rstrip()
+        counter += 1
+    stack.close()
+
 def addquote(chat_id, bot, msg):
+    global counter
     quoteFile = open('quotes.txt', 'a')
-    print(msg['reply_to_message']['text'], file = quoteFile)
-    bot.sendMessage(chat_id, "Quote added")
+    try:
+        print(msg['text'].split(" ")[1] + ";<;"+ msg['reply_to_message']['from']['username'] +";<;"+ msg['reply_to_message']['text'] , file = quoteFile)
+        #TODO: No telegran nick == no quote, should be fixed
+        bot.sendMessage(chat_id, "Quote added")
+        counter += 1
+    except:
+        bot.sendMessage(chat_id, "Error adding quote")
+
     quoteFile.close()
 
 def readquote(chat_id, bot, msg):
@@ -13,12 +32,14 @@ def readquote(chat_id, bot, msg):
     try:
         temp = msg['text'].split(" ")[1]
 
-        for i in range(0,3):
+        for i in range(counter):
             text = quoteFile.readline()
-            check = text.split(" ")[0]
-            quote = text.split(" ")[1]
+            #text = text.rstrip()
+            check = text.split(";<;")[0]
+            who = text.split(";<;")[1]
+            quote = text.split(";<;")[2] #TODO: Miten oikeesti menee?
             if check == temp:
-                bot.sendMessage(chat_id, quote)
+                bot.sendMessage(chat_id, quote + "by @" + who)
 
     except:
         bot.sendMessage(chat_id, "No such message")
