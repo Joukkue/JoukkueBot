9b15be0168bd29a48ea6ed57a386ba421473e580
joonas.malinen <joonas.malinen@hotmail.fi>
2017-07-23 23:17:59 +0300

    [IMP] quotes work throuh database

--- code/database.py
+++ code/database.py
@@ -1,40 +1,47 @@
 #coding: utf-8
 import sqlite3
-connection = sqlite3.connect('quotes.db')
-c = connection.cursor()
-
+#TODO: Error handling
 
 def createTable():
+    connection = sqlite3.connect('quotes.db', check_same_thread=False)
     c = connection.cursor()
     #if(checkTableExists()):
     c.execute('''CREATE TABLE IF NOT EXISTS quotes
             (find text, quote text, username text)''')
     connection.commit()
+    connection.close()
 
 def addToDataBase(find, quote, username):
-    t = (find, )
-    s = (quote, )
-    r = ( username, )
+    connection = sqlite3.connect('quotes.db', check_same_thread=False)
     c = connection.cursor()
     c.execute("INSERT INTO quotes(find, quote, username) VALUES (?, ?, ?)", (find, quote, username))
     #c.execute("INSERT INTO quotes(quote) VALUES (?)",s)
     #c.execute("INSERT INTO quotes(username) VALUES (?)",r)
     connection.commit()
+    connection.close()
 
-def findQuote(quote):
+def findQuote(quote, chat_id, bot):
+    connection = sqlite3.connect('quotes.db', check_same_thread=False)
     t = (quote, )
+    c = connection.cursor()
     c.execute("SELECT * FROM quotes WHERE find =?", t)
-    return c.fetchone()
+    found = c.fetchone()
+    bot.sendMessage(chat_id, found[1] + "\n-@" + found[2])
+    connection.close()
 
-createTable()
+#createTable()
+
+#connection = sqlite3.connect('quotes.db', check_same_thread=False)
+#c = connection.cursor()
+
 
 #addToDataBase("moi", "mitakullu", "@Jollla")
 
 #for row in c.execute('SELECT * FROM quotes'):
     #print (row)
 
-asd = findQuote("moi")
+#asd = findQuote("moi")
 
-print asd[1]
+#print asd[1]
 
 
