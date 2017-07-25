f916d70eada1f6c9817cfcee51c235c721f1d691
joonas.malinen <joonas.malinen@hotmail.fi>
2017-07-25 13:11:39 +0300

    [ADD]  Added help commands

---
+++ code/help.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+
+def help(bot, chat_id):
+    help = "Use /uptime to see how long the bot has been up\n" \
+           "Reply to a message, then use /insertquote anda tag for the quote to add a quote to the database\n" \
+           "To read quotes, message /readquote with the quotes the you wish to read\n" \
+           "Use /treenit to see upcoming practices for Espoo Hwarang Team\n"
+    bot.sendMessage(chat_id, help)
