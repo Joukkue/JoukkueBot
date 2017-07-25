88c2ce181c227d270e76446966675d1c2fd53fb6
joonas.malinen <joonas.malinen@hotmail.fi>
2017-07-25 12:45:09 +0300

    [IMP] Basic error handling for quotes

--- code/database.py
+++ code/database.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 import sqlite3
-#TODO: Error handling
+#TODO: Better Error handling
 
 def createTable():
     connection = sqlite3.connect('quotes.db', check_same_thread=False)
@@ -11,23 +11,40 @@ def createTable():
     connection.commit()
     connection.close()
 
-def addToDataBase(find, quote, username):
-    connection = sqlite3.connect('quotes.db', check_same_thread=False)
-    c = connection.cursor()
-    c.execute("INSERT INTO quotes(find, quote, username) VALUES (?, ?, ?)", (find, quote, username))
+def addToDataBase(msg, bot, chat_id):
+    try:
+        connection = sqlite3.connect('quotes.db', check_same_thread=False)
+        c = connection.cursor()
+        find = msg['text'].split(" ")[1]
+        quote = msg['reply_to_message']['text']
+        username = msg['reply_to_message']['from']['username']
+        c.execute("INSERT INTO quotes(find, quote, username) VALUES (?, ?, ?)", (find, quote, username))
+        connection.commit()
+        connection.close()
+        bot.sendMessage(chat_id, "Quote added succesfully")
+    except:
+        bot.sendMessage(chat_id, "Error adding quote. Please make sure you replied to a message and added a tag for quote")
+        connection.close()
     #c.execute("INSERT INTO quotes(quote) VALUES (?)",s)
     #c.execute("INSERT INTO quotes(username) VALUES (?)",r)
-    connection.commit()
-    connection.close()
 
-def findQuote(quote, chat_id, bot):
-    connection = sqlite3.connect('quotes.db', check_same_thread=False)
-    t = (quote, )
-    c = connection.cursor()
-    c.execute("SELECT * FROM quotes WHERE find =?", t)
-    found = c.fetchone()
-    bot.sendMessage(chat_id, found[1] + "\n-@" + found[2])
-    connection.close()
+def findQuote(msg, chat_id, bot):
+    try:
+        connection = sqlite3.connect('quotes.db', check_same_thread=False)
+        t = (msg['text'].split(" ")[1], )
+        try:
+            c = connection.cursor()
+            c.execute("SELECT * FROM quotes WHERE find =?", t)
+            found = c.fetchone()
+            bot.sendMessage(chat_id, found[1] + "\n-@" + found[2])
+            connection.close()
+        except:
+            bot.sendMessage(chat_id, "No such quote found")
+            connection.close()
+    except:
+        bot.sendMessage(chat_id, "Please add a quote to search for")
+        connection.close()
+
 
 #createTable()
 
