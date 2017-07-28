40a9f0a7523617957d8c59c84c43cf475794a2ed
joonas.malinen <joonas.malinen@hotmail.fi>
2017-07-28 16:58:50 +0300

    [IMP] Database works better

--- code/database.py
+++ code/database.py
@@ -1,11 +1,9 @@
 #coding: utf-8
 import sqlite3
-#TODO: Better Error handling
 
 def createTable():
-    connection = sqlite3.connect('quotes.db', check_same_thread=False)
+    connection = sqlite3.connect('joukkue.db', check_same_thread=False)
     c = connection.cursor()
-    #if(checkTableExists()):
     c.execute('''CREATE TABLE IF NOT EXISTS quotes
             (find text, quote text, username text)''')
     connection.commit()
@@ -17,20 +15,25 @@ def addToDataBase(bot, msg):
     chat_id = msg['chat']['id']
 
     try:
-        connection = sqlite3.connect('quotes.db', check_same_thread=False)
+        connection = sqlite3.connect('joukkue.db', check_same_thread=False)
         c = connection.cursor()
         find = msg['text'].split(" ")[1]
         quote = msg['reply_to_message']['text']
         username = msg['reply_to_message']['from']['username']
-        c.execute("INSERT INTO quotes(find, quote, username) VALUES (?, ?, ?)", (find, quote, username))
-        connection.commit()
-        connection.close()
-        bot.sendMessage(chat_id, "Quote added succesfully")
+        try:
+            c.execute("INSERT INTO quotes(find, quote, username) VALUES (?, ?, ?)", (find, quote, username))
+            connection.commit()
+            connection.close()
+            bot.sendMessage(chat_id, "Quote added succesfully")
+        except:
+            bot.sendMessage(chat_id, "Quote with the tag " + find + " already exists")
+            connection.commit()
+            connection.close()
+
+
     except:
         bot.sendMessage(chat_id, "Error adding quote. Please make sure you replied to a message and added a tag for quote")
         connection.close()
-    #c.execute("INSERT INTO quotes(quote) VALUES (?)",s)
-    #c.execute("INSERT INTO quotes(username) VALUES (?)",r)
 
 def findQuote(bot,msg):
     print("/findquote was calld")
@@ -38,7 +41,7 @@ def findQuote(bot,msg):
     chat_id = msg['chat']['id']
 
     try:
-        connection = sqlite3.connect('quotes.db', check_same_thread=False)
+        connection = sqlite3.connect('joukkue.db', check_same_thread=False)
         t = (msg['text'].split(" ")[1], )
         try:
             c = connection.cursor()
@@ -53,20 +56,17 @@ def findQuote(bot,msg):
         bot.sendMessage(chat_id, "Please add a quote to search for")
         connection.close()
 
+def listQuotes(bot, msg):
+    print("/listquotes was calld")
 
-#createTable()
-
-#connection = sqlite3.connect('quotes.db', check_same_thread=False)
-#c = connection.cursor()
-
-
-#addToDataBase("moi", "mitakullu", "@Jollla")
-
-#for row in c.execute('SELECT * FROM quotes'):
-    #print (row)
-
-#asd = findQuote("moi")
-
-#print asd[1]
-
-
+    chat_id = msg['chat']['id']
+    try:
+        connection = sqlite3.connect('joukkue.db', check_same_thread=False)
+        c = connection.cursor()
+        text = ""
+        for row in c.execute('SELECT find FROM quotes'):
+            text += row[0]
+            text += "\n"
+        bot.sendMessage(chat_id, text)
+    except:
+        bot.sendMessage(chat_id, "Error listing quotes")
