76a80328bbf4ffe3d7d8db2505eead48dd95a6c4
Unknown <joonas.malinen@hotmail.fi>
2017-08-06 21:38:01 +0300

    [FIX] Fix for gitissues

--- code/quote.py
+++ code/quote.py
@@ -2,7 +2,7 @@
 import os
 import sqlite3
 
-db_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'resources\joukkue.db')
+db_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'joukkue.db')
 
 
 def main():
