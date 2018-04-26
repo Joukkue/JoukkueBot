3325f0f79e9f9f3c39d0a186eaad0d3bea2e1a8f
Unknown <joonas.malinen@hotmail.fi>
2018-04-26 20:11:07 +0300

    Update for message forwarding

--- resources/archive.py
+++ resources/archive.py
@@ -4,5 +4,6 @@ joukkueBot = '364939368:AAEoiv-I8bQgid0vR2n0SV5W2oG9HdEKANk'
 testBot = '344427823:AAEuGoOkLJ_bMa-WVhPg7Toephtt_f8y91M'
 testBoobs = 'AgADBAADNKoxGztVqFADOE0YQfkum7FSvRkABFE7KNRBe5in-e0AAgI'
 testChat_id = '-247837959'
-testground = '-184405992'
+testground = '-1001158753083'
+
 joukkue = '-1001059712421'
